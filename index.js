21beb9cf37d69c72b28e7db24dfd637a5d50b692
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2021-03-22 16:39:43 -0400

    Question Array

---
+++ index.js
@@ -0,0 +1,100 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readme = "README.md";
+const generateMarkdown = require("./Assets/generateMD");
+
+// Array of questions
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your Project?"
+        
+    },
+
+    {
+        type: "input", 
+        name: "description",
+        message: "Give a brief discription of your Project: "
+       
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "How is your project installed? "
+        
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "How does someone use your project? "
+        
+    },
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please list the contributors for this project? "
+        
+    },
+
+    {
+        type: "input",
+        name: "testing",
+        message: "Please provide test instructions for this project? "
+    },
+
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Which license did you use for this project:",
+        choices: [
+            "Apache_License_2.0",
+            "GNU_AGPLv3",
+            "GNU_GPLv3",
+            "GNU_LGPLv3",
+            "ISC",
+            "MIT",
+            "The_Unlicense"
+        ],
+        
+    },
+
+    {
+        type: "input",
+        name: "gitName",
+        message: "What is your GitHub Username? "
+        
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email? "
+        
+    }
+
+
+];
+
+// Function to create README
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+        return console.log(err);
+      }
+      else{
+        return console.log("Success! Your README.md file has been generated")
+      }
+    });
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(userdata => writeToFile(readme, userdata));
+}
+
+// function call to initialize program
+init();
